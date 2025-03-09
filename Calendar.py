81a372d05c6949d766ea256eccd236bcee3fd54b
CatarineSales <catarinesales2013@gmail.com>
2025-03-09 15:46:52 -0300

    Comitando a ferramenta calendário

---
+++ Calendar.py
@@ -0,0 +1,21 @@
+#******************************************************************
+
+#Diadema, 13  de  Agosto  de  2024 
+#Projeto Simples  -  Calendário
+#Linguagem: Python
+
+#******************************************************************
+
+#DESCRIÇÃO: Proj. que é possivel visualizar o Mês e o ano da sua 
+#preferencia.
+#Para acessar, é possivel buscando o arquivo no CMD e logo depois 
+#apertando ENTER para executar.
+
+#******************************************************************
+
+import calendar
+aa = 2024 #ano
+mm = 8   #Mês
+
+#Mostrabdo o calendário
+print(calendar.month(aa, mm))
